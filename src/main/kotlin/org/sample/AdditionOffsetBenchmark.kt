2232617f8d6dac3f3a5b545043898139790f9f1c
dievsky <dievsky@gmail.com>
2021-02-01 15:39:40 +0100

    Added arithmetic benchmarks with offset
    The idea is that adding an offset significantly impacts
    the performance
    
    (cherry picked from commit d4b51c2ce56eeda0c7b4cd09231bd268a64fd190)

---
+++ src/main/kotlin/org/sample/AdditionOffsetBenchmark.kt
@@ -0,0 +1,56 @@
+package org.sample
+
+import org.jetbrains.bio.viktor.F64Array
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+
+@Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(2)
+@State(Scope.Benchmark)
+open class AdditionOffsetBenchmark {
+
+    @Param("100", "1000", "10000", "100000", "1000000", "10000000")
+    var arraySize: Int = 0
+
+    lateinit var s: MathStructures
+    lateinit var viktor2DArray1: F64Array
+    lateinit var viktor2DArray2: F64Array
+
+    var iteration = 0
+
+    @Setup
+    fun setup() {
+        s = MathStructures(HUGE_ARRAY_SIZE)
+        viktor2DArray1 = s.viktorArray1.reshape(HUGE_ARRAY_SIZE / arraySize, arraySize)
+        viktor2DArray2 = s.viktorArray2.reshape(HUGE_ARRAY_SIZE / arraySize, arraySize)
+    }
+
+    @Setup(Level.Invocation)
+    fun inc() {
+        iteration++
+        if ((iteration + 1) * arraySize >= HUGE_ARRAY_SIZE) {
+            iteration = 0
+        }
+    }
+
+    @Benchmark
+    fun viktor(bh: Blackhole) {
+        val viktorArray1 = viktor2DArray1.V[iteration]
+        val viktorArray2 = viktor2DArray2.V[iteration]
+        bh.consume(viktorArray1 + viktorArray2)
+    }
+
+    @Benchmark
+    fun loop(bh: Blackhole) {
+        val src1 = s.src1
+        val src2 = s.src2
+        val offset = iteration * arraySize
+        bh.consume(DoubleArray(arraySize) { src1[it + offset] + src2[it + offset] })
+    }
+
+    companion object {
+        const val HUGE_ARRAY_SIZE = 10_000_000
+    }
+}
